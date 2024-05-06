d24fc08f615008e57046135d2ffc6202636b1bfc
Nguyen Hoang Phuc <81422862+Phuc262002@users.noreply.github.com>
2024-05-06 17:53:57 +0700

    chore: Remove commented out login route

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,3 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-
-Route::get('/login', function () {
-    return response()->json([
-        "status" => false,
-        "message" => "Unauthorized",
-    ], 401);
-})->name('login');
