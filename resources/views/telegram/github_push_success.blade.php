f6b702d7d290044c3d1bce3e1f2a019987eb785b
Nguyen Hoang Phuc <81422862+Phuc262002@users.noreply.github.com>
2024-06-17 12:37:55 +0700

    test-deploy

--- resources/views/telegram/github_push_success.blade.php
+++ resources/views/telegram/github_push_success.blade.php
@@ -3,4 +3,4 @@
 👨🏻‍💻 <b>[{{ $message['developer']['name'] }}]</b>(<a href="{{ $message['developer']['html_url'] }}">{{ $message['developer']['html_url'] }}</a>)
 
 <b>Name: </b> {{ $message['workflow']['name'] }}
-<b>Head: </b> {{ $message['workflow']['head_branch'] }}
+<b>Head: </b> {{ $message['workflow']['head_branch'] }} 
